7f1ffc0b9cb79123a52875fc9778775c5b0ab9ec
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-04-20 21:40:20 -0300

    feat(main) add interface for appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,15 +1,31 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
-import { startOfHour, parseISO } from 'date-fns';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(401)
+      .json({ message: 'This appointment is already booked' });
+  }
 
   const appointment = {
     id: uuid(),
