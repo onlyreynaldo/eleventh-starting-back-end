884e23f58eebc0aa5ceacb68dc071054b32b8f54
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-06-23 00:23:37 -0300

    feat(upload) add route static for view files

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
