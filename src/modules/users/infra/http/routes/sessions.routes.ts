6d5ddc1f394caaf192eee4bb0981acd4931287fb
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-06-23 23:05:55 -0300

    feat(users) refuting user module with Inver. Dep.

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
 const usersRouter = Router();
+const usersRepository = new UsersRepository();
 
 usersRouter.post('/', async (request, response) => {
   const { email, password } = request.body;
 
-  const sessionUser = new AuthenticateUserService();
+  const sessionUser = new AuthenticateUserService(usersRepository);
 
   const { user, token } = await sessionUser.execute({ email, password });
 
