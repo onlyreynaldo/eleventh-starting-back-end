bbd747c161a4955ea5ede563afa7caf198bf3f1f
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-06-30 00:23:02 -0300

    feat(mail) add resetPassword testing Insomnia

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -31,11 +31,11 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
     await this.mailProvider.sendMail(
       email,
-      'Pedido de recuperação de senha recibido.',
+      `Pedido de recuperação de senha recibido: ${token}`,
     );
   }
 }
