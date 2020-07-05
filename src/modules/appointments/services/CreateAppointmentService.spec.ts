91df27bd6e0b5cf8d0e84f8d579c6266c4136fda
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-07-04 22:14:19 -0300

    perf(appointments): add spy on for test passed on same time
    add jest spyOn for passed test in future

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -30,6 +30,10 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 6, 5, 11).getTime();
+    });
+
     const appointmentDate = new Date(2020, 6, 5, 12);
 
     await createAppointment.execute({
